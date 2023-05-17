490d3e4e7c873debcfdf5fda23522e2f262abdfc
Fede Mosquera <freddymdq@gmail.com>
2023-05-17 13:22:49 -0300

    Proyecto Final 2 Agregando ViewsManager y quitando los metodos dentro de la viewsRoutes

--- src/router/views.routes.js
+++ src/router/views.routes.js
@@ -1,11 +1,31 @@
 import { Router } from "express";
-//import AccesManager from "../Dao/controllers/AccesManager.js";
-import productModel from "../Dao/models/products.model.js";
-import cartModel from "../Dao/models/cart.model.js";
+import ViewsManager from "../Dao/controllers/ViewsManager.js";
 
 const router = Router();
-//const accesManager = new AccesManager()
+
 router.get('/', async (req, res) => {
+  await ViewsManager.renderHome(req, res);
+});
+
+router.get('/products', async (req, res) => {
+  await ViewsManager.renderProducts(req, res);
+});
+
+router.get('/product/:id', async (req, res) => {
+  await ViewsManager.renderProduct(req, res);
+});
+
+router.get('/cart/:id', async (req, res) => {
+  await ViewsManager.renderCart(req, res);
+});
+
+export default router;
+
+
+
+// routes viejos con los metodos dentro los dejo por las dudas
+
+/* router.get('/', async (req, res) => {
   const { page = 1, limit, query } = req.query;
   const opt = { page, limit: parseInt(limit) || 16, lean: true };
   opt.sort = { price: -1 };
@@ -26,8 +46,8 @@ router.get('/', async (req, res) => {
     products,
    
   });
-});
-
+}); */
+/* 
 router.get('/products', async (req, res) => {
   try {
     let { page = 1, limit, query, sort, category } = req.query;
@@ -75,21 +95,19 @@ router.get('/product/:id', async (req, res) => {
     const productId = req.params.id;
     const product = await productModel
       .findById(productId)
-      .lean();
-      
+      .lean();    
     if (!product) {
       return res.status(204).end();
     }
-
     res.render('product', { product });
   } catch (error) {
     console.error(error);
     res.status(500).send({ error: 'Error interno del servidor' });
   }
 });
-
+ */
 // vista cart
-router.get("/cart/:id", async (req, res) => {
+/* router.get("/cart/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const cart = await cartModel
@@ -102,17 +120,12 @@ router.get("/cart/:id", async (req, res) => {
       if (cart === 'favicon.ico') {
         return res.status(204).end();
       }
-
     // Suma los precios de los productos en el carrito
     const total = cart.products.reduce((acc, product) => {
       return acc + product.product.price * product.quantity;
     }, 0);
-
     res.render("cart", { title: "Carrito", cart, total });
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
-});
-
-export default router;
-
+}); */
